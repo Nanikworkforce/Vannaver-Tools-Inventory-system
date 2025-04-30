8ea1dd30f39b5806825ae192da85408f8144b04d
unknown <scandiclamar@gmail.com>
2025-04-30 12:03:18 +0000

    edits

--- app/dashboard/tools/page.tsx
+++ app/dashboard/tools/page.tsx
@@ -15,59 +15,59 @@ const ToolsPage = () => {
           {/* Column 1 */}
           <div className="flex flex-col gap-4">
             <Link href="/dashboard/open">
-              <div className="bg-gray-800/80 text-white p-6 rounded text-lg cursor-pointer hover:bg-gray-700/80 transition-colors">
+              <div className="bg-gray-800/80 text-white p-6 rounded text-lg cursor-pointer hover:bg-gray-700/80 transition-colors flex items-center justify-center text-center">
                 4 - ½ Fat Man
               </div>
             </Link>
-            <div className="bg-gray-800/80 text-white p-6 rounded text-lg">
+            <div className="bg-gray-800/80 text-white p-6 rounded text-lg flex items-center justify-center text-center">
               HiMars ESP w/Fatman ESP Desander
             </div>
-            <div className="bg-gray-800/80 text-white p-6 rounded text-lg">
+            <div className="bg-gray-800/80 text-white p-6 rounded text-lg flex items-center justify-center text-center">
               2-3/8" Thorman
             </div>
-            <div className="bg-gray-800/80 text-white p-6 rounded text-lg">
+            <div className="bg-gray-800/80 text-white p-6 rounded text-lg flex items-center justify-center text-center">
               Backflushable Valve
             </div>
           </div>
 
           {/* Column 2 */}
           <div className="flex flex-col gap-4">
-            <div className="bg-gray-800/80 text-white p-6 rounded text-lg">
+            <div className="bg-gray-800/80 text-white p-6 rounded text-lg flex items-center justify-center text-center">
               4 - ½ Fat Man 239
             </div>
-            <div className="bg-gray-800/80 text-white p-6 rounded text-lg">
+            <div className="bg-gray-800/80 text-white p-6 rounded text-lg flex items-center justify-center text-center">
               HiMars RP w/Fatman ESP Desander
             </div>
-            <div className="bg-gray-800/80 text-white p-6 rounded text-lg">
+            <div className="bg-gray-800/80 text-white p-6 rounded text-lg flex items-center justify-center text-center">
               5" Fat Man KI
             </div>
-            <div className="bg-gray-800/80 text-white p-6 rounded text-lg">
+            <div className="bg-gray-800/80 text-white p-6 rounded text-lg flex items-center justify-center text-center">
               Operation Sandstone
             </div>
           </div>
 
           {/* Column 3 */}
           <div className="flex flex-col gap-4">
-            <div className="bg-gray-800/80 text-white p-6 rounded text-lg">
+            <div className="bg-gray-800/80 text-white p-6 rounded text-lg flex items-center justify-center text-center">
               MK 45
             </div>
-            <div className="bg-gray-800/80 text-white p-6 rounded text-lg">
+            <div className="bg-gray-800/80 text-white p-6 rounded text-lg flex items-center justify-center text-center">
               C-RAM 4-1/2" 13 CR
             </div>
-            <div className="bg-gray-800/80 text-white p-6 rounded text-lg">
+            <div className="bg-gray-800/80 text-white p-6 rounded text-lg flex items-center justify-center text-center">
               3" Fat Man KI
             </div>
           </div>
 
           {/* Column 4 */}
           <div className="flex flex-col gap-4">
-            <div className="bg-gray-800/80 text-white p-6 rounded text-lg">
+            <div className="bg-gray-800/80 text-white p-6 rounded text-lg flex items-center justify-center text-center">
               Fatman ESP Desander
             </div>
-            <div className="bg-gray-800/80 text-white p-6 rounded text-lg">
+            <div className="bg-gray-800/80 text-white p-6 rounded text-lg flex items-center justify-center text-center">
               C-RAM 4"
             </div>
-            <div className="bg-gray-800/80 text-white p-6 rounded text-lg">
+            <div className="bg-gray-800/80 text-white p-6 rounded text-lg flex items-center justify-center text-center">
               Little Boy 4-1/2"
             </div>
           </div>
