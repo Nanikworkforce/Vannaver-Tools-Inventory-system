f09ce78e49a758701e06c8a829e36913b82f7e0d
unknown <scandiclamar@gmail.com>
2025-04-30 14:55:04 +0000

    edits

--- components/dashboard/dashboard-sidebar.tsx
+++ components/dashboard/dashboard-sidebar.tsx
@@ -49,11 +49,11 @@ export function DashboardSidebar() {
       icon: Boxes,
       href: "/dashboard/equipment/",
     },
-    {
-      title: "Tool Monitoring",
-      icon: Timer,
-      href: "/dashboard/tool-monitoring",
-    },
+    // {
+    //   title: "Tool Monitoring",
+    //   icon: Timer,
+    //   href: "/dashboard/tool-monitoring",
+    // },
     {
       title: "Inventory & Tool Management",
       icon: Wrench,
